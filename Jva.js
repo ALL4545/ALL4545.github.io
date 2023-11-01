966d9c50189870df8c4da0df548b2cff29b003ca
ALL4545 <148864191+ALL4545@users.noreply.github.com>
2023-11-01 16:19:06 +0100

    Delete Jva.js

--- Jva.js
+++
@@ -1,9 +0,0 @@
-const eye = document.querySelector('.feather-eye');
-const eyeoff = document.querySelector('.feather-eye-off');
-const passwordField = document.querySelector('input[type=]'); 
-
-eye.addEventListener('click , () => {
-    eye.computedStyleMap.display = "none" ; 
-    eyeoff.computedStyleMap.display = "block"
-    passwordField.type = "text" ; 
-})
